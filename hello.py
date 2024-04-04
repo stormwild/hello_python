d1432082c044e8f3385ad2cfae4c0cd5160317cb
Alexander R Torrijos <artorrijos@gmail.com>
2024-04-04 19:20:38 +0800

    String and List Basics

--- hello.py
+++ hello.py
@@ -1,1 +1,56 @@
-print("Hello World");
+message = "Hello Python"
+print(message)
+
+name = "ada lovelace"
+
+print(name.title())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+
+greeting_ada = f"Hello, {full_name.title()}!"
+print(greeting_ada)
+
+
+x, y, z = 0, 0, 0
+
+numbers = f"{x} {y} {z}"
+print(numbers)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+
+print(motorcycles)
+
+motorcycles.append('ducati')
+
+print(motorcycles)
+
+del motorcycles[3]
+
+print(motorcycles)
+
+motorcycles.append('ducati')
+
+ducati = motorcycles.pop()
+
+print(ducati)
+
+print(motorcycles)
+
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+print(cars)
+
+# cars.sort()
+# cars.sort(reverse=True)
+# print(cars)
+
+print(sorted(cars))
+
+print(cars)
+
+print(cars.reverse())
+
+print(len(cars))
